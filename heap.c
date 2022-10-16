8c0bd1ea083fda4bae5e70ae976ccd59eadd5c74
Replit user <>
2022-10-16 17:05:46 +0000

    new try

--- heap.c
+++ heap.c
@@ -31,18 +31,7 @@ void* heap_top(Heap* pq){
 
 
 void heap_push(Heap* pq, void* data, int priority){
-  pq->size--;
-  pq->heapArray[0] = pq->heapArray[pq->size];
-  int priority=pq->heapArray[0].priority;
-  int now = 1;
-  while((now<=pq->size && pq->heapArray[now].priority) > priority)|| (now+1 <= pq > size && pq->heapArray[now+1].priority > priority)){
-    heapElem tmp=pq->heapArray[(now-1)/2];
-    if(now+1 <= pq->size && pq-> heapArray[now].priority < pq->heapArray[now+1].priority) now++;
 
-    pq->heapArray[(now-1)/2]=pq->heapArray[now];
-    pq->heapArray[now]=tmp;
-    now=now*2 +1;
-  }
 
 
   
@@ -51,6 +40,17 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
+  pq->size--;
+  pq->heapArray[0] = pq->heapArray[pq->size];
+  int priority=pq->heapArray[0].priority;
+  int now = 1;
+  while((now<=pq->size && pq->heapArray[now].priority) > priority)|| (now+1 <= pq -> size && pq->heapArray[now+1].priority > priority)){
+    heapElem tmp=pq->heapArray[(now-1)/2];
+    if(now+1 <= pq->size && pq-> heapArray[now].priority < pq->heapArray[now+1].priority) now++;
+
+    pq->heapArray[(now-1)/2]=pq->heapArray[now];
+    pq->heapArray[now]=tmp;
+    now=now*2 +1;
 
   
 
