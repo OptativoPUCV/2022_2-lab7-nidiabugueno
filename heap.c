da5cd4d9998f5b47b1974f5344a06cca90e4c78b
Replit user <>
2022-10-16 17:02:08 +0000

    new try

--- heap.c
+++ heap.c
@@ -21,7 +21,7 @@ void* heap_top(Heap* pq){
   if(pq == NULL || pq->heapArray == NULL){
     return NULL
   }
-  if(pq->size ! =0){
+  if(pq->size != 0){
     return pq->heapArray[0].data;
   }
   return NULL;
@@ -38,8 +38,18 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
-  size--;
-  array[0] = array[pq->size];
+  pq->size--;
+  pq->heapArray[0] = pq->heapArray[pq->size];
+  int priority=pq->heapArray[0].priority;
+  int now = 1;
+  while((now<=pq->size && pq->heapArray[now].priority) > priority) || (now+1 <= pq > size && pq->heapArray[now+1].priority > priority)){
+    heapElem tmp=pq->heapArray[(now-1)/2];
+    if(now+1 <= pq->size && pq-> heapArray[now].priority < pq->heapArray[now+1].priority) now++;
+
+    pq->heapArray[(now-1)/2]=pq->heapArray[now];
+    pq->heapArray[now]=tmp;
+    now=now*2 +1;
+  }
 
   
 
